4a161f0b9ac97a4956ddbe01723f709f18656782
Cerebreo <nellirism@outlook.com>
2021-08-07 15:59:11 -0700

    Updates

---
+++ precache-manifest.c0d84b95181ed870a09a2314c50c84d2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bedc27be1f903fe18dc6b8640747a62c",
+    "url": "/kindkoders/index.html"
+  },
+  {
+    "revision": "9c54e38e0d3bd772061e",
+    "url": "/kindkoders/static/css/2.bd39f990.chunk.css"
+  },
+  {
+    "revision": "b187b883b72cf723e27c",
+    "url": "/kindkoders/static/css/main.20c84c83.chunk.css"
+  },
+  {
+    "revision": "9c54e38e0d3bd772061e",
+    "url": "/kindkoders/static/js/2.e0bb53ac.chunk.js"
+  },
+  {
+    "revision": "b187b883b72cf723e27c",
+    "url": "/kindkoders/static/js/main.7147c0a5.chunk.js"
+  },
+  {
+    "revision": "1889ea456385419043c1",
+    "url": "/kindkoders/static/js/runtime~main.0ab0bcb9.js"
+  }
+]);
